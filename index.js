e1e2c27e6e6b4c726dde910f3abc7e3bd4631564
shoibaldas <shoibalshantu@gmail.com>
2022-11-09 02:40:13 +0600

    get data of services from db is completed

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express')
-const app = express();
 var cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vrrg6hy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const servicesCollection = client.db('serviceMan').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(error => console.log(error))
 
 app.get('/', (req, res) => {
     res.send('Graphics Bari API is running');
